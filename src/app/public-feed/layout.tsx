320edeb2b48b59bceb9a3871318c83232ec8782c
retsaeiouu <rets1nonly@gmail.com>
2024-10-11 01:48:54 +0800

    db pre-config

--- src/app/public-feed/layout.tsx
+++ src/app/public-feed/layout.tsx
@@ -2,19 +2,20 @@
 //    setup neon db lesgoooooooo
 //    deploy if it works
 
+import { CreateButton } from "@/components/client/CreateButton";
 import Link from "next/link";
 
 //    set up db and finish the creating anonymous notes feature
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex flex-col h-screen w-screen bg-custom_raisin overflow-auto">
-      <header className="bg-custom_raisin/60 backdrop-blur-lg sticky top-0 left-0 w-full z-1 flex flex-col justify-center py-3 md:py-4 px-8 md:px-16 lg:px-32">
+      <header className="bg-custom_raisin/60 backdrop-blur-lg sticky top-0 left-0 w-full z-1 flex items-center py-3 md:py-4 px-8 md:px-16 lg:px-32">
         <Link href="/">
-          <h1 className="translate-y-[-6px] text-custom_mint font-gloria font-bold text-2xl md:text-3xl tracking-wider">
+          <h1 className="translate-y-[-6px] text-custom_mint font-gloria font-bold text-2xl md:text-3xl tracking-wider hover:text-custom_columbia transition-all duration-500 ease-in-out">
             lapagan
           </h1>
         </Link>
-        {/*  TODO: add create post button */}
+        <CreateButton />
       </header>
       {children}
       <footer className="mt-auto ml-auto pt-10 py-1 mr-2">
---
+++ src/components/client/CreateButton.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { PaperAirplaneIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export function CreateButton() {
+  "use client";
+  return (
+    <div
+      className={`${usePathname() === "/public-feed" ? "" : "hidden"} ml-auto text-custom_mint hover:text-custom_columbia transition-all duration-500 ease-in-out`}
+    >
+      <Link
+        href="/public-feed/create-anonymous-note"
+        className="flex gap-1 items-center"
+      >
+        <h4 className="text-base font-bold font-poppins tracking-wide">
+          create
+        </h4>
+        <PaperAirplaneIcon className="h-5 w-5" />
+      </Link>
+    </div>
+  );
+}
